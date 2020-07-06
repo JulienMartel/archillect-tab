959748b194a857ee95adc829c9ed1899df370af0
ju <julienbmartel@gmail.com>
2020-07-06 00:11:51 -0500

    almost ready

--- bg.js
+++ bg.js
@@ -1,8 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-	chrome.storage.sync.get({"bgvalue": "#222222"}, bgCallback);
-});
-var bgCallback = function(list) {
-var color = list["bgvalue"];
-console.log(color);
-document.body.style.background = color;
-};
+fetch("https://api.unsplash.com/photos/random?client_id=ZwlAH167E4UEIulH5zobX29b8MCvfj1dga_IF0iB2ik").then(res => res.json())
+	.then(result => {
+		console.log(result)
+		const { urls: {full : url} } = result
+		document.querySelector("img").src = url
+	})
