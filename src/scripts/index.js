290fb7378fce0bf3bf22cab0b46c333ee67c2b7b
ju <julienbmartel@gmail.com>
2022-11-24 01:28:39 -0600

    1.4.9 shipped

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,49 +1,47 @@
-const $ = query => document.querySelector(query)
+const $ = (query) => document.querySelector(query);
 
 // for versions
-const setVersionTag = version => {
-  $("a.version").textContent = version
-}
-const checkVersion = version => {
+const setVersionTag = (version) => {
+  $("a.version").textContent = version;
+};
+const checkVersion = (version) => {
   chrome.storage.local.get(["version"], ({ version: currentVersion }) => {
     if (currentVersion != version) {
       // open them up to a new page
-      const current = window.location.href
+      const current = window.location.href;
 
       window.close();
-      window.open('/newupdate.html', '_blank');
-      window.open(current, '_blank');
-      chrome.storage.local.set({ version })
+      window.open("/newupdate.html", "_blank");
+      window.open(current, "_blank");
+      chrome.storage.local.set({ version });
     }
-  })
-}
-const url = chrome.runtime.getURL('./data.json');
+  });
+};
+const url = chrome.runtime.getURL("./data.json");
 fetch(url)
   .then((response) => response.json())
-  .then(({updates}) => {
-    checkVersion(updates[0].version)
-    setVersionTag(updates[0].version)
-  })
+  .then(({ updates }) => {
+    checkVersion(updates[0].version);
+    setVersionTag(updates[0].version);
+  });
 
+const bg = $(".bg");
+const logo = $(".logo");
+const setNewImg = (url) => {
+  document.body.style.backgroundImage = `url(${url})`;
+  bg.src = url;
+  logo.style.display = "block";
+};
 
-
-const bg = $(".bg")
-const logo = $(".logo")
-const setNewImg = url => {
-  document.body.style.backgroundImage = `url(${url})`
-  bg.src = url
-  logo.style.display = "block"
-}
-
-chrome.storage.local.get(['src'], ({src}) => {
+chrome.storage.local.get(["src"], ({ src }) => {
   if (src) {
-    setNewImg(src)
+    setNewImg(src);
   } else {
-    fetch("https://archillect-recent.vercel.app")
-      .then(res => res.json())
+    fetch("https://unofficial-archillect-api.vercel.app/recent/1")
+      .then((res) => res.json())
       .then(({ src }) => {
-        setNewImg(src)
-        chrome.storage.local.set({ src })
-      })
+        setNewImg(src);
+        chrome.storage.local.set({ src });
+      });
   }
-})
+});
--- src/scripts/updates.js
+++ src/scripts/updates.js
@@ -1,51 +1,50 @@
-const setVersionTag = version => {
-  document.querySelector(".version").textContent = version
-}
+const setVersionTag = (version) => {
+  document.querySelector(".version").textContent = version;
+};
 
-const url = chrome.runtime.getURL('./data.json');
+const url = chrome.runtime.getURL("./data.json");
 fetch(url)
   .then((response) => response.json())
-  .then(({updates}) => {
-    renderInfo(updates)
-    setVersionTag(updates[0].version)
-  })
+  .then(({ updates }) => {
+    renderInfo(updates);
+    setVersionTag(updates[0].version);
+  });
 
+const renderInfo = (updates) => {
+  const cont = document.querySelector(".cont");
+  updates.forEach(({ version, whatsNew, means }, i) => {
+    const h2 = document.createElement("H2");
+    h2.innerText = `Version ${version + (i == 0 ? " 🎊" : "")}`;
+    const h3 = document.createElement("H3");
+    h3.innerText = "Whats new?";
+    const ul = document.createElement("UL");
 
-const renderInfo = updates => {
-  const cont = document.querySelector(".cont")
-  updates.forEach(({version, whatsNew, means}, i) => {
-    const h2 = document.createElement("H2")
-    h2.innerText = `Version ${version + (i == 0 ? " 🎊" : "")}`
-    const h3 = document.createElement("H3")
-    h3.innerText = "Whats new?"
-    const ul = document.createElement("UL")
-    
-    whatsNew.forEach(n => {
-      const x = document.createElement("LI")
-      x.innerText = n
-      ul.appendChild(x)
-    })
-    cont.appendChild(h2)
-    cont.appendChild(h3)
-    cont.appendChild(ul)
+    whatsNew.forEach((n) => {
+      const x = document.createElement("LI");
+      x.innerText = n;
+      ul.appendChild(x);
+    });
+    cont.appendChild(h2);
+    cont.appendChild(h3);
+    cont.appendChild(ul);
 
     if (means?.length) {
-      const h3_2 = document.createElement("H3")
-      h3_2.innerText = "What does this mean?"
-      const ul2 = document.createElement("UL")
+      const h3_2 = document.createElement("H3");
+      h3_2.innerText = "What does this mean?";
+      const ul2 = document.createElement("UL");
 
-      means.forEach(m => {
-        const x = document.createElement("LI")
-        x.innerText = m
-        ul2.appendChild(x)
-      })
-      cont.appendChild(h3_2)
-      cont.appendChild(ul2)
+      means.forEach((m) => {
+        const x = document.createElement("LI");
+        x.innerText = m;
+        ul2.appendChild(x);
+      });
+      cont.appendChild(h3_2);
+      cont.appendChild(ul2);
     }
 
-    const div = document.createElement("DIV")
-    div.className = "divider"
+    const div = document.createElement("DIV");
+    div.className = "divider";
 
-    if (i !== updates.length - 1) cont.appendChild(div)
-  })
-}
+    if (i !== updates.length - 1) cont.appendChild(div);
+  });
+};
