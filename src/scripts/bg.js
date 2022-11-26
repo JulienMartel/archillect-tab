ed15132ebcd20b7f3156740a2027e4267c761ad7
ju <julienbmartel@gmail.com>
2022-11-26 13:42:37 -0600

    1.5.0

--- src/scripts/bg.js
+++ src/scripts/bg.js
@@ -1,11 +1,14 @@
-chrome.alarms.get("getNewImg", alarm => {
+chrome.alarms.get("getNewImg", (alarm) => {
   if (!alarm) {
-    chrome.alarms.create("getNewImg", {periodInMinutes: 10, when: Date.now() + 1})
+    chrome.alarms.create("getNewImg", {
+      periodInMinutes: 10,
+      when: Date.now() + 1,
+    });
   }
-})
+});
 
 chrome.alarms.onAlarm.addListener(() => {
-  fetch("https://archillect-recent.vercel.app")
-    .then(res => res.json())
-		.then(({ src }) => chrome.storage.local.set({ src }))
-})
+  fetch("https://unofficial-archillect-api.vercel.app/recent/1")
+    .then((res) => res.json())
+    .then(({ src }) => chrome.storage.local.set({ src }));
+});
